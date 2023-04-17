8c9cfc6335da9f74fc2465c495bc4a737fc8790e
Nikolay Volegov <kolyavolegov@gmail.com>
2023-04-17 21:52:17 +0300

    not merge2

--- Special_app/src/GitMerge.java
+++ Special_app/src/GitMerge.java
@@ -1,5 +1,6 @@
 public class GitMerge {
     public static void main(String[] args) {
         System.out.println("Давай поробуем замержить с конфликтом ветки");
+        System.out.println("Это мой второй комит");
     }
 }
